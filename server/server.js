9bba318eb88443e99e203ddd46fd67f423f200d6
Stanley Phu <phu.stanley@gmail.com>
2018-07-18 02:44:10 -0700

    Init setup

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('new user');
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
